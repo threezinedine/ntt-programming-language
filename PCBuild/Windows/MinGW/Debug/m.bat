4f09b43fbb26c67de55d98f72edad6bde671c5f3
threezinedine <threezinedine@gmail.com>
2025-07-27 18:11:22 +0700

    remove unnecessary mingw cmake

--- PCBuild/Windows/MinGW/Debug/m.bat
+++ PCBuild/Windows/MinGW/Debug/m.bat
@@ -3,13 +3,14 @@
 set current_dir=%cd%
 set project_dir=%current_dir%\..\..\..\..
 set build_dir=%project_dir%\build\Windows\MinGW\Debug
-set example_dir=%build_dir%\examples
+set example_dir=%build_dir%
 
 cd %example_dir%
 echo %cd%
 for %%f in (%example_dir%\*.exe) do (
-    echo %%f
-    %%f
+    IF %%f NEQ %example_dir%\NTTLanguage_test.exe (
+        %%f
+    )
 )
 cd %current_dir%
 
